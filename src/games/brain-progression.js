9042ad807547c2c9f0ce8c9b25e22e05cb8d126e
Zhenya30000 <eugeny.sverdlov@gmail.com>
2022-08-26 17:31:11 +0500

    Fix Brain Progression game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -7,23 +7,20 @@ const rules = 'What number is missing in the progression?';
 
 const generateRound = () => {
   const questionAnswerPair = [];
+  const questions = [];
   let nextItem = getRandomInRange(90);
   const increment = getRandomInRange(10, 1);
   const arrayLength = getRandomInRange(10, 5);
-  const hiddenSymbolIndex = getRandomInRange(arrayLength, 2);
-  questionAnswerPair.push([`${nextItem}`]);
-  for (let i = 0; i < arrayLength - 1; i += 1) {
-    if (i === hiddenSymbolIndex) {
-      questionAnswerPair[0] += ' ..';
-      nextItem += increment * 2;
-      questionAnswerPair.push(`${nextItem - increment}`);
-      questionAnswerPair[0] += ` ${nextItem}`;
-      i += 2;
-    }
+  let answer = 0;
+  const hiddenSymbolIndex = getRandomInRange(arrayLength - 1);
+  for (let i = 0; i <= arrayLength - 1; i += 1) {
+    questions.push(nextItem);
     nextItem += increment;
-    questionAnswerPair[0] += ` ${nextItem}`;
   }
-  questionAnswerPair[0] = questionAnswerPair[0].toString();
+  answer = `${questions[hiddenSymbolIndex]}`;
+  questions[hiddenSymbolIndex] = '..';
+  questionAnswerPair.push(questions.join(' '));
+  questionAnswerPair.push(answer);
   return questionAnswerPair;
 };
 
