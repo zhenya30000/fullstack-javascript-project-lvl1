67a05005a695b2e0547fee59a307cecd9c0537de
Zhenya30000 <eugeny.sverdlov@gmail.com>
2022-08-30 17:38:15 +0500

    Fix linter problem

--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,3 @@
-export default (max, min = 0) => { return Math.floor((Math.random() * (max - min + 1)) + min); };
+const getRandomInRange = (max, min = 0) => Math.floor(Math.random() * (max - min + 1) + min);
+
+export default getRandomInRange;
