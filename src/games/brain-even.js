da50ea391dc12ed784a56cef1a1d331a3deeabc0
zhenya30000 <eugeny.sverdlov@gmail.com>
2022-09-05 15:16:17 +0500

    Fix Brain-Even game

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,12 +3,13 @@ import getRandomInRange from '../helpers.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => ((number % 2 === 0) ? 'yes' : 'no');
+
 const generateRound = () => {
-  const questionAnswerPair = [];
-  questionAnswerPair.push(getRandomInRange(100));
-  const isEven = (questionAnswerPair[0] % 2 === 0) ? 'yes' : 'no';
-  questionAnswerPair.push(isEven);
-  return questionAnswerPair;
+  const randomNumber = (getRandomInRange(100));
+  const question = `${randomNumber}`;
+  const answer = isEven(randomNumber);
+  return [question, answer];
 };
 
 export default () => startGame(rules, generateRound);
