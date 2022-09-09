acf17825f7eb66decf2f59e1a264b68cbf23fbda
Zhenya30000 <eugeny.sverdlov@gmail.com>
2022-09-09 12:30:33 +0500

    Fix predicate functions

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,12 +3,12 @@ import getRandomInRange from '../helpers.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (number) => ((number % 2 === 0) ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const randomNumber = (getRandomInRange(100));
   const question = `${randomNumber}`;
-  const answer = isEven(randomNumber);
+  const answer = isEven(randomNumber) === true ? 'yes' : 'no';
   return [question, answer];
 };
 
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,16 +6,16 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (number) => {
   for (let j = 2; j < number; j += 1) {
     if (number % j === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const generateRound = () => {
   const randomNumber = getRandomInRange(100, 3);
   const question = `${randomNumber}`;
-  const answer = isPrime(randomNumber);
+  const answer = isPrime(randomNumber) === true ? 'yes' : 'no';
   return [question, answer];
 };
 
